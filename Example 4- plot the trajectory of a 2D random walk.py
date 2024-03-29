4fa2666d11c0597610256d9a0a38e9de7dc839c2
brettnakao <bnakao@lion.lmu.edu>
2024-03-29 10:27:06 -0700

    commit homework

--- Example 4- plot the trajectory of a 2D random walk.py
+++ Example 4- plot the trajectory of a 2D random walk.py
@@ -8,14 +8,22 @@ Created on Mon Mar 25 15:19:39 2024
 
 import numpy as np
 
-rng = np.random.default_rng()
+rng = np.random.default_rng(42)
 
 # X-step
 x_sample = rng.random(500)
 x_step = 2 * (x_sample > .5) - 1
-x_coord = np.sum(x_step)
+x_coord = np.cumsum(x_step)
 
 # Y-step
 y_sample = rng.random(500)
 y_step = 2 * (y_sample > .5) - 1
-y_coord = np.sum(y_step)
+y_coord = np.cumsum(y_step)
+
+# Plot coordinates
+import matplotlib.pyplot as plt
+plt.plot(x_coord, y_coord)
+plt.title('2D Random Walk')
+plt.xlabel('x-step')
+plt.ylabel('y-step')
+plt.show()
